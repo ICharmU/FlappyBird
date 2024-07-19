1a6f3506416736a0d9803525641d1850072dca85
Sean <seanisloveseanislife@gmail.com>
2024-07-19 14:44:52 -0700

    Pillar created in desired height range

--- flappy_bird/flappy_bird_scratch.py
+++ flappy_bird/flappy_bird_scratch.py
@@ -1,4 +1,4 @@
-import arcade, random
+import arcade, random, copy
 
 #initialize game
 SCREEN_WIDTH = 800
@@ -6,17 +6,19 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Flappy Bird"
 
 RECT_WIDTH = 100
-RECT_HEIGHT = 300
-RECT_COLOR = arcade.color.BLUE
+RECT_HEIGHT = SCREEN_HEIGHT
+RECT_COLOR = arcade.color.BLUE #bottom
+RECT_COLOR_2 = arcade.color.BLACK #top
 
 BACKGROUND_COLOR = arcade.color.RED
 
 class Pipe:
     def __init__(self):
-        self.x = SCREEN_WIDTH
-        self.y = SCREEN_HEIGHT * (1 + 8*random.random()) / 10
+        self.x = SCREEN_WIDTH/2
+        self.y = SCREEN_HEIGHT * (-0.3 + 0.4 * random.random())
         self.change_x = 0
         self.change_y = 0
+        self.i = 1
 
     def update(self):
         self.x += self.change_x
@@ -26,13 +28,11 @@ class Pipe:
 
     def refill(self):
         self.x = SCREEN_WIDTH
-        self.y = SCREEN_HEIGHT * (1 + 8*random.random()) / 10
+        self.y = SCREEN_HEIGHT * (-0.3 + 0.4 * random.random())
 
     def draw(self):
         arcade.draw_rectangle_filled(self.x, self.y, RECT_WIDTH, RECT_HEIGHT, RECT_COLOR)
 
-        
-            
 class Bird:
     pass
 
@@ -40,19 +40,36 @@ class Game(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        self.pipe = Pipe()
+        self.pipe_bottom = Pipe()
+        self.pipe_top = copy.deepcopy(self.pipe_bottom)
+        self.pipe_top.y = self.pipe_bottom.y+1.2*SCREEN_HEIGHT
+
         self.background_color = BACKGROUND_COLOR
 
     def on_update(self, delta_time):
-        self.pipe.update()
+        self.pipe_top.update()
+        self.pipe_bottom.update() #update to match top pipe
 
     def on_draw(self):
         self.clear()
-        self.pipe.draw()
+        self.pipe_bottom.draw()
+        self.pipe_top.draw()
+        #arcade.draw_rectangle_filled(test.x, test.y, RECT_WIDTH, RECT_HEIGHT, arcade.color.PURPLE)
+        #arcade.draw_rectangle_filled(test_2.x, test_2.y, RECT_WIDTH, RECT_HEIGHT, arcade.color.ORANGE)
+
+#test pipe height for placement        
+'''
+test = Pipe()
+test.y = -0.3*SCREEN_HEIGHT
+test_2 = copy.deepcopy(test)
+test_2.y = 1.3*SCREEN_HEIGHT
+
+'''
 
 def main():
     Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
+    
     print('Game started')
 
 if __name__ == '__main__':
